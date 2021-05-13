928a958d62d69096ba9aea7e0a44bd9e34d69004
lazyduo <dadanuna@gmail.com>
2021-05-13 22:24:26 +0900

    upload ui.py

---
+++ ui/ui.py
@@ -0,0 +1,8 @@
+from PyQt5 import QtWidgets, uic
+import sys
+
+class Ui(QtWidgets.QMainWindow):
+    def __init__(self):
+        super(Ui, self).__init__()
+        uic.loadUi('main_v1.0.ui', self)
+        self.show()
